43cab5d3a7f9ada64a5e9e7c537a0c413dfadcef
TheREALAdr <95942707+TheREALAdr@users.noreply.github.com>
2023-05-03 14:13:45 +0000

    Added base elements to grid. Pycharm test later?

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ window.config(padx=50, pady=50)
 # Canvas Initialization
 canvas = Canvas(width=800, height=526, highlightthickness=)
 canvas.create_image(400, 263, image=card_front.png)
-# ^ Add keywords for the first two numbers? Hard to understand if
-# you don't know how canvas.create_image(kw**) works.
 canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
 canvas.create_text(400, 263, text="Title", font=(FONT_NAME, 60, "bold"))
 
@@ -32,8 +30,7 @@ correct_image = PhotoImage(file="images/correct.png")
 incorrect_button = Button(image=incorrect_image, highlightthickness=0)
 correct_button = Button(image=correct_image, highlightthickness=0)
 
-# Padding
-incorrect_image.grid(column=0, row=1)
-correct_image.grid(column=1, row=1)
-canvas.grid
-#Ending here, need to finish the gridding and keywords for the canvas. Might do that later.
+# Adding elements to the grid
+incorrect_button.grid(column=0, row=1, sticky=EW)
+correct_button.grid(column=1, row=1, sticky=EW)
+canvas.grid(column=0, row=0, columnspan=2)
