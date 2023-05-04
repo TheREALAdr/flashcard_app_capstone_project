f425c3d7f37d0cf8fafd761075da3370873ef1a0
TheREALAdr <95942707+TheREALAdr@users.noreply.github.com>
2023-05-04 10:07:42 -0500

    Update main.py
    Changed background color for the canvas.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # Canvas Initialization
-canvas = Canvas(width=800, height=526, highlightthickness=0)
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 canvas.create_image(400, 263, image=card_front.png)
 title = canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
 word = canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
