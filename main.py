ee8b0551a10e217ffd0a1d79cbb3c82daff5599d
TheREALAdr <95942707+TheREALAdr@users.noreply.github.com>
2023-05-04 10:04:44 -0500

    Update main.py
    Changed background color for window.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ FONT_NAME = "Ariel"
 # Window Initialization
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # Canvas Initialization
 canvas = Canvas(width=800, height=526, highlightthickness=0)
