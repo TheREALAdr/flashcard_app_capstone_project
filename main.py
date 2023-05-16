a58c35ef755e8f262da1dfb887af076a2335bc19
TheREALAdr <anotidamurambadoro455@gmail.com>
2023-05-15 19:36:43 -0500

    Fixed a LOT of problems with the running of the project (one import was messing with EVERYTHING). Some words from .csv file are bugging.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter.ttk import *
 import pandas
 import random
 
@@ -16,15 +15,15 @@ FONT_NAME = "Arial"
 
 # ------------------------------ WORD GENERATION --------------------- #
 
-with open(file=data/french_words.csv) as words_file:
-    words_data = pandas.read_csv()
+with open(file="data/french_words.csv") as words_file:
+    words_data = pandas.read_csv(words_file)
     dict_of_words = words_data.to_dict(orient="records")
 
 def generate_foreign_word():
     global random_word
     random_word = random.choice(dict_of_words)
-    foreign_translation = random_word[”French”]
-    known_translation = random_word[”English”]
+    foreign_translation = random_word["French"]
+    known_translation = random_word["English"]
     canvas.itemconfig(language, text="French")
     canvas.itemconfig(word, text=f"{foreign_translation}")
     
@@ -41,21 +40,22 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# Images
+card_front_image = PhotoImage(file="images/card_front.png")
+red_x_image = PhotoImage(file="images/incorrect.png")
+green_check_image = PhotoImage(file="images/correct.png")
+
 # Canvas Initialization
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 flashcard = canvas.create_image(400, 263, image=card_front_image)
 language = canvas.create_text(400, 150, text="Language", font=(FONT_NAME, 40, "italic"))
 word = canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
 
-# Images
-card_front_image = PhotoImage(file="images/card_front.png")
-red_x_image = PhotoImage(file="images/incorrect.png")
-green_check_image = PhotoImage(file="images/correct.png")
 
 
 # Buttons
-unknown_button = Button(image=red_x_image, command=generate_foreign_word, highlightthickness=0, activebackground=BACKGROUND_COLOR, borderwidth=0)
-known_button = Button(image=green_check_image, highlightthickness=0, activebackground=BACKGROUND_COLOR, borderwidth=0)
+unknown_button = Button(image=red_x_image, command=generate_foreign_word, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, highlightthickness=0, bd=0,)
+known_button = Button(image=green_check_image, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, highlightthickness=0, bd=0)
 
 # Adding elements to the grid
 unknown_button.grid(column=0, row=1, sticky=EW)
