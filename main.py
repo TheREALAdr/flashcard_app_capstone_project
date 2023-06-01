622cb60c7866b79e14b8358a0697c1eaf7c496ac
TheREALAdr <anotidamurambadoro455@gmail.com>
2023-06-01 11:27:41 -0500

    DUDEEEE!!!! YOU'RE FINALLY DONE! EVERYTHING WORKS NOW! If you want to do the challenge part later, fine by me.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pandas
 import random
+import csv
 
 # This project currently only works with French and English. Later on, see if you can add
 # functionality that lets you grab 100 most popular words of ANY language from
@@ -19,43 +20,48 @@ with open(file="data/french_words.csv") as words_file:
     words_data = pandas.read_csv(words_file)
     dict_of_words = words_data.to_dict(orient="records")
 
-current_card = {}
+current_word = {}
+
+
 def generate_words():
-    global current_card, flip_timer
+    global current_word, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(dict_of_words)
-    next_card()
+    current_word = random.choice(dict_of_words)
+    next_word()
     flip_timer = window.after(3000, flip_card)
 
 
-
-def next_card():
-    foreign_translation = current_card["French"]
+# Moving to another card
+def next_word():
+    foreign_translation = current_word["French"]
     canvas.itemconfig(flashcard, image=card_front_image)
     canvas.itemconfig(language, text="French", fill="black")
     canvas.itemconfig(word, text=f"{foreign_translation}", fill="black")
 
 
-
+# Flipping the current card
 def flip_card():
     canvas.itemconfig(flashcard, image=card_back_image)
-    known_translation = current_card["English"]
+    known_translation = current_word['English']
     canvas.itemconfig(language, text="English", fill="white")
     canvas.itemconfig(word, text=f"{known_translation}", fill="white")
 
 
-
-
-
-
-
-# known_translation is currently a useless variable, but
-# keeping here as a building block for a function where
-# I’ll need it. Will probably delete it soon.
-
-# Also, see if you can use random_word later on WITHOUT
-# needing the global designation.
-
+# ------------------------------ REMOVING WORDS/ADDING TO OTHER FILES --------------------- #
+def generate_and_remove_words():
+    try:
+        dict_of_words.remove(current_word)
+        words_to_learn = pandas.DataFrame(data=dict_of_words)
+        words_to_learn.to_csv("words_to_learn.csv", index=False)
+        # Might be a simpler way to do this, check the pandas.Dataframe.to_csv() method later.
+    except ValueError:
+        pass
+    # except FileNotFoundError:
+    #     words_to_learn_file = open("words_to_learn.csv", mode="w")
+    #     with open("words_to_learn.csv", mode="a") as words_to_learn_file:
+    #         words_to_learn_file.write(f"\n{current_word}")
+    finally:
+        generate_words()
 
 
 # ------------------------------ USER INTERFACE/VARIABLES ----------------------------- #
@@ -83,7 +89,8 @@ word = canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
 # Buttons
 unknown_button = Button(image=red_x_image, command=generate_words, bg=BACKGROUND_COLOR,
                         activebackground=BACKGROUND_COLOR, highlightthickness=0, bd=0, )
-known_button = Button(image=green_check_image, command=generate_words, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR,
+known_button = Button(image=green_check_image, command=generate_and_remove_words, bg=BACKGROUND_COLOR,
+                      activebackground=BACKGROUND_COLOR,
                       highlightthickness=0, bd=0)
 
 # Adding elements to the grid
